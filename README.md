ce697993dc32b05d966e83e65699f999662d1259
Vishwa <VB086617>
2022-01-30 22:55:24 +0530

    Changes to add Swagger and some formatting

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # RabbitMQHandsOn
+
+The Swagger UI Link can be accessed using : http://{servcer:port}/swagger-ui.html
--- producer/src/main/java/com/rmq/producer/App.java
+++ producer/src/main/java/com/rmq/producer/App.java
@@ -1,7 +1,17 @@
 package com.rmq.producer;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Hello world!
@@ -9,11 +19,35 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableSwagger2
 public class App 
 {
     public static void main( String[] args )
     {
     	SpringApplication.run(new Object[] { App.class }, args);
 
+    }
+    
+    /**
+	 * Configuration for Swagger
+	 *
+	 * @return a Docket object
+	 */
+    @Bean
+    public Docket swaggerConfiguration() {
+    	return new Docket(DocumentationType.SWAGGER_2)
+    			.select()
+    			.paths(PathSelectors.ant("/testrmq/*"))
+    			.apis(RequestHandlerSelectors.basePackage("com.rmq.producer"))
+    			.build()
+    			.apiInfo(apiDetails());
+    }
+    
+    private ApiInfo apiDetails() {
+    	return new ApiInfo(
+    			"Rabbit MQ Producer API", 
+    			"Rabbit MQ Message Producer", 
+    			null, 
+    			"null", null, null, null, Collections.emptyList());
     }
 }
